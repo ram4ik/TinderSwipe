cc64cd1e7038ee61b013de3e15af2d32a2677b3d
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-03-25 22:22:25 +0200

    Initial Commit

---
+++ TinderSwipe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinderSwipe
+//
+//  Created by Ramill Ibragimov on 25.03.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
